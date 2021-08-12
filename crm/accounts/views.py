46301bbc90bcac671592f12525276540c0e0f19d
Gonzalo Villamandos <82946513+gonzalov85@users.noreply.github.com>
2021-08-11 22:21:41 -0300

    le estaba mostrando a leo

---
+++ crm/accounts/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def home(request):
+    return render(request,'accounts/dashboard.html')
+
+def products(request):
+    return render(request,'accounts/products.html')
+
+def costumer(request):
+    return render(request,'accounts/costumer.html')
+
+def contact(request):
+    return HttpResponse("Contact page")
+
+
